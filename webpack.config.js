bfe3e17735a5318ca6884c6a5ef1c030c268e181
Hack Reactor Student <student@hackreactor.com>
2018-07-18 16:44:50 -0700

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/react-client/src');
+const DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      },
+      {
+        test: /\.css$/,
+        include: SRC_DIR,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
