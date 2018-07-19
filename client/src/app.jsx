7cdcaf65644af472aafad615609fb1bbda194dcf
Hack Reactor Student <student@hackreactor.com>
2018-07-18 20:00:41 -0700

    setting up ajax call

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -6,14 +6,20 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      images: []
     }
+  }
+
+  getImages() {
+    $.get('/images', (data) => {
+      this.setState({images: data});
+    });
   }
 
   render() {
     return (
       <div>
-        Hi
+        <img src={`https://s3-us-west-1.amazonaws.com/fec-photo-gallery-images/${this.state.images[0]}`}></img>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-let app = express();
+const db = require('./database');
+const app = express();
+
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 let port = 3002;
+
+app.get('/images', (req, res) => {
+
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}...`);
