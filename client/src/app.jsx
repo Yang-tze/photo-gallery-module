818abf6e16dcaba9cf2c331036590a19ba851f9d
Hack Reactor Student <student@hackreactor.com>
2018-07-18 19:22:52 -0700

    finished setting up jest

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -6,14 +6,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+
     }
   }
 
   render() {
     return (
       <div>
-
+        Hi
       </div>
     );
   }
