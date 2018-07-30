636a4fe32e39ca3c0283e2548d683e817f0c73df
Michael Lin <michaellin@Michaels-MacBook-Pro.local>
2018-07-30 10:26:15 -0700

    static rendering polished up

--- client/src/components/main-image.jsx
+++ client/src/components/main-image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainImage = (props) => (
   <div className='main-image'>
-    <img className='image' src="https://s3-us-west-1.amazonaws.com/fec-photo-gallery-images/sandisk_16gb_02.jpg"></img>
+    <img className='image' src={`https://s3-us-west-1.amazonaws.com/fec-photo-gallery-images/${props.image}`} onClick={() => props.showModal()}></img>
   </div>
 )
 
